45b0866d204615b4c77ecbd818557f06478afb71
ravi07777 <ravindrax7878@gmail.com>
2026-04-07 08:42:02 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,19 +2,19 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22579742"))
-API_HASH = environ.get("API_HASH", "2817067278f743a7f57e4dbea3e0d031")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", ")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "cricketlinksipl")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/cricketlinksipl")  # Channel link
+FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", ")  # Channel username without @, 
+FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "")  # Channel link
 
 # Admin Configuration
-ADMINS = list(map(int, environ.get("ADMINS", "1380815395").split()))
+ADMINS = list(map(int, environ.get("ADMINS", "").split()))
 
 # Optional: Bot Owner ID
-OWNER_ID = int(environ.get("OWNER_ID", "1380815395"))
+OWNER_ID = int(environ.get("OWNER_ID", ""))
 
 # Database URL (if you want to add database support later)
 DATABASE_URL = environ.get("DATABASE_URL", "")
